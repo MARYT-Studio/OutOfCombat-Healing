b6a1739e4a35e10192c8821d834b33b631665a33
RisingInIris2017 <risinginiris2017@gmail.com>
2024-09-15 21:14:03 +0800

    Alpha version

--- src/main/java/world/maryt/out_of_combat/OutOfCombat.java
+++ src/main/java/world/maryt/out_of_combat/OutOfCombat.java
@@ -1,18 +1,67 @@
 package world.maryt.out_of_combat;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import world.maryt.out_of_combat.handler.OutOfCombatHandler;
+
+import java.io.File;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class OutOfCombat {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
+    public static long noAttackingTimeThreshold = Long.MAX_VALUE;
+    public static long noAttackedTimeThreshold = Long.MAX_VALUE;
+    public static long outOfCombatTimeThreshold = Long.MAX_VALUE;
+
+    public static boolean DEBUG = false;
+
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+    public void preInit(FMLPreInitializationEvent preEvent) {
+        Configuration config = new Configuration(new File(Loader.instance().getConfigDir(), Tags.MOD_ID + ".cfg"));
+        try {
+            config.load();
+            {
+                Property property = config.get(Configuration.CATEGORY_GENERAL, "noAttackingTimeThreshold", Integer.MAX_VALUE);
+                property.setComment("Out-of-combat timer only counts after No-attacking timer counts more than this value.");
+                OutOfCombat.noAttackingTimeThreshold = property.getLong();
+                property.setShowInGui(true);
+            }
+            {
+                Property property = config.get(Configuration.CATEGORY_GENERAL, "noAttackedTimeThreshold", Integer.MAX_VALUE);
+                property.setComment("Out-of-combat timer only counts after Not-being-attacked timer counts more than this value.");
+                OutOfCombat.noAttackedTimeThreshold = property.getLong();
+                property.setShowInGui(true);
+            }
+            {
+                Property property = config.get(Configuration.CATEGORY_GENERAL, "outOfCombatTimeThreshold", Integer.MAX_VALUE);
+                property.setComment("When Out-of-combat timer counts exceeds this value, the player is considered out of combat.");
+                OutOfCombat.outOfCombatTimeThreshold = property.getLong();
+                property.setShowInGui(true);
+            }
+            {
+                Property property = config.get("Debug", "Debug", false);
+                property.setComment("Enable this for debugging purpose");
+                OutOfCombat.DEBUG = property.getBoolean();
+                property.setShowInGui(false);
+            }
+
+            LOGGER.info("No Mob Friendly Fire - configuration loaded.");
+        } finally {
+            config.save();
+        }
     }
 
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new OutOfCombatHandler());
+    }
 }
---
+++ src/main/java/world/maryt/out_of_combat/handler/OutOfCombatHandler.java
@@ -0,0 +1,104 @@
+package world.maryt.out_of_combat.handler;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+import static world.maryt.out_of_combat.OutOfCombat.*;
+import static world.maryt.out_of_combat.Tags.MOD_ID;
+
+public class OutOfCombatHandler {
+
+    private static NBTTagCompound getOutOfCombatData(EntityPlayer player) {
+        NBTTagCompound forgeData = player.getEntityData();
+        if (!(forgeData.hasKey("PlayerPersisted"))) {
+            forgeData.setTag("PlayerPersisted", new NBTTagCompound());
+        }
+        NBTTagCompound persistedData = forgeData.getCompoundTag("PlayerPersisted");
+        if (!(persistedData.hasKey(MOD_ID))) {
+            persistedData.setTag(MOD_ID, new NBTTagCompound());
+        }
+        return persistedData.getCompoundTag(MOD_ID);
+    }
+
+    // Clear when player is attacking
+    // Event counts only if it is finally fired without being cancelled.
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onPlayerAttacking(LivingAttackEvent event) {
+        if (event.getSource().getTrueSource() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) event.getSource().getTrueSource();
+            NBTTagCompound outOfCombatData = getOutOfCombatData(player);
+            outOfCombatData.setLong("noAttackingTime", 0L);
+            if (DEBUG) LOGGER.debug("noAttackingTime cleared.");
+            outOfCombatData.setLong("outOfCombatTime", 0L);
+            if (DEBUG) LOGGER.debug("outOfCombatTime cleared.");
+        }
+    }
+
+
+    // Clear when player is being attacked
+    // Event counts only if it is finally fired without being cancelled.
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onPlayerAttacked(LivingDamageEvent event) {
+        if (event.getEntityLiving() instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+            NBTTagCompound outOfCombatData = getOutOfCombatData(player);
+            outOfCombatData.setLong("noAttackedTime", 0L);
+            if (DEBUG) LOGGER.debug("noAttackedTime cleared.");
+            outOfCombatData.setLong("outOfCombatTime", 0L);
+            if (DEBUG) LOGGER.debug("outOfCombatTime cleared.");
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.side.isServer() && event.phase == TickEvent.Phase.END) {
+            NBTTagCompound outOfCombatData = getOutOfCombatData(event.player);
+
+            long newNoAttackingTime = outOfCombatData.getLong("noAttackingTime") + 1;
+            long newNoAttackedTime = outOfCombatData.getLong("noAttackedTime") + 1;
+
+            outOfCombatData.setLong("noAttackingTime", newNoAttackingTime);
+            outOfCombatData.setLong("noAttackedTime", newNoAttackedTime);
+
+            // Stop Out-of-combat Timer.
+            // If this timer has not been initialized, it will be initialized with a 0 value.
+            // If this timer has got a zero value, this line effectively do nothing.
+            // If this timer has a nonzero value, it will function as a countdown timer.
+//            outOfCombatData.setLong("stopOutOfCombatTimer", Long.max(0L, outOfCombatData.getLong("stopOutOfCombatTimer") - 1L));
+            if (DEBUG) LOGGER.debug("stopOutOfCombatTimer: {}, It should be set to: {}",
+                    outOfCombatData.getLong("stopOutOfCombatTimer"),
+                    Long.max(0L, outOfCombatData.getLong("stopOutOfCombatTimer") - 1L));
+
+            if (outOfCombatData.getLong("noAttackingTime") >= noAttackingTimeThreshold &&
+                    outOfCombatData.getLong("noAttackedTime") >= noAttackedTimeThreshold &&
+                    outOfCombatData.getLong("stopOutOfCombatTimer") <= 0L
+            ) {
+                long newOutOfCombatTime = outOfCombatData.getLong("outOfCombatTime") + 1L;
+                outOfCombatData.setLong("outOfCombatTime", newOutOfCombatTime);
+                if (DEBUG && outOfCombatData.getLong("outOfCombatTime") >= outOfCombatTimeThreshold)
+                    LOGGER.info("Player is out of combat for {} ticks.",
+                            outOfCombatData.getLong("outOfCombatTime") - outOfCombatTimeThreshold);
+            } else {
+                if (DEBUG) {
+                    LOGGER.debug("Out-of-combat timer's ticking is stopped due to: noAttackingTime = {}, noAttackedTime = {}, stopOutOfCombatTimer = {}.",
+                        outOfCombatData.getLong("noAttackingTime"),
+                        outOfCombatData.getLong("noAttackedTime"),
+                        outOfCombatData.getLong("stopOutOfCombatTimer")
+                    );
+                }
+            }
+
+            // Debug info here
+            if (DEBUG) LOGGER.debug("noAttackingTime: {}/{}, noAttackedTime: {}/{}, outOfCombatTime: {}/{} (Current/Threshold).",
+                    outOfCombatData.getLong("noAttackingTime"), noAttackingTimeThreshold,
+                    outOfCombatData.getLong("noAttackedTime"), noAttackedTimeThreshold,
+                    outOfCombatData.getLong("outOfCombatTime"), outOfCombatTimeThreshold
+            );
+        }
+    }
+}
