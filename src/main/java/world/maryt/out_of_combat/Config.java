713d79a80f3f941129ce3c518bbf0c4c03e8cc5f
RisingInIris2017 <risinginiris2017@gmail.com>
2024-09-17 16:16:08 +0800

    Release 1.1.0

--- src/main/java/world/maryt/out_of_combat/Config.java
+++ src/main/java/world/maryt/out_of_combat/Config.java
@@ -1,51 +1,44 @@
 package world.maryt.out_of_combat;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = OutOfCombat.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER.comment("Whether to log the dirt block on common setup").define("logDirtBlock", true);
+    private static final ForgeConfigSpec.BooleanValue DEBUG = BUILDER.comment("Enable this for debugging purpose").define("DEBUG", false);
 
-    private static final ForgeConfigSpec.IntValue MAGIC_NUMBER = BUILDER.comment("A magic number").defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER.comment("What you want the introduction message to be for the magic number").define("magicNumberIntroduction", "The magic number is... ");
-
-    // a list of strings that are treated as resource locations for items
-    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER.comment("A list of items to log on common setup.").defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+    private static final ForgeConfigSpec.LongValue NO_ATTACKED_TIME_THRESHOLD = BUILDER.comment("Out-of-combat timer only counts after Not-being-attacked timer counts more than this value.")
+            .defineInRange("noAttackedTimeThreshold",
+                    Long.MAX_VALUE,
+                    0,
+                    Long.MAX_VALUE);
+    private static final ForgeConfigSpec.LongValue NO_ATTACKING_TIME_THRESHOLD = BUILDER.comment("Out-of-combat timer only counts after No-attacking timer counts more than this value.")
+            .defineInRange("noAttackingTimeThreshold",
+                    Long.MAX_VALUE,
+                    0,
+                    Long.MAX_VALUE);
+    private static final ForgeConfigSpec.LongValue OUT_OF_COMBAT_THRESHOLD = BUILDER.comment("When Out-of-combat timer counts exceeds this value, the player is considered out of combat.")
+            .defineInRange("outOfCombatTimeThreshold",
+                    Long.MAX_VALUE,
+                    0,
+                    Long.MAX_VALUE);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
-    public static boolean logDirtBlock;
-    public static int magicNumber;
-    public static String magicNumberIntroduction;
-    public static Set<Item> items;
-
-    private static boolean validateItemName(final Object obj) {
-        return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
-    }
+    public static boolean debug;
+    public static long noAttackingTimeThreshold;
+    public static long noAttackedTimeThreshold;
+    public static long outOfCombatTimeThreshold;
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
-        logDirtBlock = LOG_DIRT_BLOCK.get();
-        magicNumber = MAGIC_NUMBER.get();
-        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
-
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream().map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName))).collect(Collectors.toSet());
+        debug = DEBUG.get();
+        noAttackedTimeThreshold = NO_ATTACKED_TIME_THRESHOLD.get();
+        noAttackingTimeThreshold = NO_ATTACKING_TIME_THRESHOLD.get();
+        outOfCombatTimeThreshold = OUT_OF_COMBAT_THRESHOLD.get();
     }
 }
---
+++ src/main/java/world/maryt/out_of_combat/handler/OutOfCombatHandler.java
@@ -0,0 +1,118 @@
+package world.maryt.out_of_combat.handler;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import static world.maryt.out_of_combat.Config.*;
+import static world.maryt.out_of_combat.OutOfCombat.MODID;
+import static world.maryt.out_of_combat.OutOfCombat.LOGGER;
+
+public class OutOfCombatHandler {
+    private static CompoundTag getOutOfCombatData(Player player) {
+        CompoundTag forgeData = player.getPersistentData();
+        if (!(forgeData.contains(Player.PERSISTED_NBT_TAG))) {
+            forgeData.put(Player.PERSISTED_NBT_TAG, new CompoundTag());
+        }
+        CompoundTag persistedData = forgeData.getCompound(Player.PERSISTED_NBT_TAG);
+        if (!(persistedData.contains(MODID))) {
+            persistedData.put(MODID, new CompoundTag());
+        }
+        return persistedData.getCompound(MODID);
+    }
+
+    // Clear when player is attacking
+    // Event counts only if it is finally fired without being cancelled.
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onPlayerAttacking(LivingAttackEvent event) {
+        if (event.getSource().getEntity() instanceof Player player) {
+            CompoundTag outOfCombatData = getOutOfCombatData(player);
+            outOfCombatData.putLong("noAttackingTime", 0L);
+            if (debug) LOGGER.debug("noAttackingTime cleared.");
+            outOfCombatData.putLong("outOfCombatTime", 0L);
+            if (debug) LOGGER.debug("outOfCombatTime cleared.");
+        }
+    }
+
+
+    // Clear when player is being attacked
+    // Event counts only if it is finally fired without being cancelled.
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onPlayerAttacked(LivingDamageEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            CompoundTag outOfCombatData = getOutOfCombatData(player);
+            outOfCombatData.putLong("noAttackedTime", 0L);
+            if (debug) LOGGER.debug("noAttackedTime cleared.");
+            outOfCombatData.putLong("outOfCombatTime", 0L);
+            if (debug) LOGGER.debug("outOfCombatTime cleared.");
+        }
+    }
+
+    // On death, clear all other timers, but save the "stop" countdown timer.
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onPlayerDeath(LivingDeathEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            CompoundTag outOfCombatData = getOutOfCombatData(player);
+            outOfCombatData.putLong("noAttackingTime", 0L);
+            outOfCombatData.putLong("noAttackedTime", 0L);
+            outOfCombatData.putLong("outOfCombatTime", 0L);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.player.isAlive() && event.phase == TickEvent.Phase.END && event.side.isServer()) {
+
+            CompoundTag outOfCombatData = getOutOfCombatData(event.player);
+
+            long newNoAttackingTime = outOfCombatData.getLong("noAttackingTime") + 1;
+            long newNoAttackedTime = outOfCombatData.getLong("noAttackedTime") + 1;
+
+            outOfCombatData.putLong("noAttackingTime", newNoAttackingTime);
+            outOfCombatData.putLong("noAttackedTime", newNoAttackedTime);
+
+            // Stop Out-of-combat Timer.
+            // If this timer has not been initialized, it will be initialized with a 0 value.
+            // If this timer has got a zero value, this line effectively do nothing.
+            // If this timer has a nonzero value, it will function as a countdown timer.
+            outOfCombatData.putLong("stopOutOfCombatTimer", Long.max(0L, outOfCombatData.getLong("stopOutOfCombatTimer") - 1L));
+            if (debug) LOGGER.debug("stopOutOfCombatTimer: {}, It should be set to: {}",
+                    outOfCombatData.getLong("stopOutOfCombatTimer"),
+                    Long.max(0L, outOfCombatData.getLong("stopOutOfCombatTimer") - 1L));
+
+            if (outOfCombatData.getLong("noAttackingTime") >= noAttackingTimeThreshold &&
+                    outOfCombatData.getLong("noAttackedTime") >= noAttackedTimeThreshold &&
+                    outOfCombatData.getLong("stopOutOfCombatTimer") <= 0L
+            ) {
+                long newOutOfCombatTime = outOfCombatData.getLong("outOfCombatTime") + 1L;
+                outOfCombatData.putLong("outOfCombatTime", newOutOfCombatTime);
+                if (debug && outOfCombatData.getLong("outOfCombatTime") >= outOfCombatTimeThreshold)
+                    LOGGER.info("Player is out of combat for {} ticks.",
+                            outOfCombatData.getLong("outOfCombatTime") - outOfCombatTimeThreshold);
+            } else {
+                if (debug) {
+                    LOGGER.debug("Out-of-combat timer's ticking is stopped due to: noAttackingTime = {}, noAttackedTime = {}, stopOutOfCombatTimer = {}.",
+                            outOfCombatData.getLong("noAttackingTime"),
+                            outOfCombatData.getLong("noAttackedTime"),
+                            outOfCombatData.getLong("stopOutOfCombatTimer")
+                    );
+                }
+            }
+
+            // Debug info here
+            if (debug) {
+                LOGGER.debug("noAttackingTime: {}/{}, noAttackedTime: {}/{}, outOfCombatTime: {}/{} (Current/Threshold).",
+                        outOfCombatData.getLong("noAttackingTime"), noAttackingTimeThreshold,
+                        outOfCombatData.getLong("noAttackedTime"), noAttackedTimeThreshold,
+                        outOfCombatData.getLong("outOfCombatTime"), outOfCombatTimeThreshold
+                );
+            }
+
+        }
+    }
+}
